179c18038493b3ffc3b32c85c7c2e697a7498e20
MonkeyPikachu <68237959+MonkeyPikachu@users.noreply.github.com>
2020-07-13 20:43:01 +0530

    Updated to Github

---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+ls -l   >tempfile
+count=`wc -l <tempfile`
+echo $count
+echo " Please Enter  you guess for the numbe of files in the current directory"
+read  userGuess
+
+while [[ $count -ne  $userGuess ]]; do
+	echo " Checking if your guess is correct or not???"
+	if [[ $count -lt $userGuess ]]
+	then
+		echo "Ohh!!! You have entered a guess value that is actually more then the real answer. Please enter  again."
+        read  userGuess
+    fi
+	if [[ $count  -gt $userGuess ]]
+	then
+		echo "Ohh!!! You have entered a guess value that is actually less  then the real answer. Please enter  again."
+        read  userGuess
+	fi
+	if [[ $count  -eq $userGuess ]]
+	then
+	    echo "Correct answer good. Bye Bye"
+	    exit
+	fi
+done
+
+
+echo "Correct answer good'
